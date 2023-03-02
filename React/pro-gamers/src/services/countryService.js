b3be2c3dea52fff8a5193901cdea8953e5cb867a
Arnar Leifsson <aleifsson@deloitte.is>
2023-03-02 20:08:27 +0000

    Fixing untracked files.

--- React/pro-gamers/src/services/countryService.js
+++ React/pro-gamers/src/services/countryService.js
@@ -1,23 +1,30 @@
 /*
     Service to provide data for country, region and city
 **/
-import fetch from 'fetch-jsonp';
 
-const endpoint = 'http://battuta.medunes.net/api';
-const key = '00000000000000000000000000000000';
+const endpoint = "http://battuta.medunes.net/api";
+const key = "00000000000000000000000000000000";
 
-const countryService = () => {
-	return {
-		getCountries: (format) => {
-			return fetch(`${endpoint}/country/all/?key=${key}`).then(resp => resp.json()).then(countries => countries.map(c => format(c)));
-		},
-		getRegions: (countryCode = 'is', format) => {
-			return fetch(`${endpoint}/region/${countryCode}/all/?key=${key}`).then(resp => resp.json()).then(regions => regions.map(r => format(r)));
-		},
-		getCities: (countryCode = 'is', regionName = '', format) => {
-			return fetch(`${endpoint}/city/${countryCode}/search/?region=${regionName}&key=${key}`).then(resp => resp.json()).then(cities => cities.map(c => format(c)));
-		}
-	};
-};
+async function retrieveItemsWithFormat(url, format) {
+  const response = await fetch(url);
+  const countries = await response.json();
+  return countries.map((c) => format(c));
+}
 
-export default countryService();
+export async function getCountries(format) {
+  return retrieveItemsWithFormat(`${endpoint}/country/all/?key=${key}`, format);
+}
+
+export async function getRegions(countryCode = "is", format) {
+  return retrieveItemsWithFormat(
+    `${endpoint}/region/${countryCode}/all/?key=${key}`,
+    format
+  );
+}
+
+export async function getCities(countryCode = "is", regionName = "", format) {
+  return retrieveItemsWithFormat(
+    `${endpoint}/city/${countryCode}/search/?region=${regionName}&key=${key}`,
+    format
+  );
+}
